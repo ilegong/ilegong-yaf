5b1eeecd3d8d95c4c1ae9602818be29294f936a9
shichaopeng <shichaopeng@ilegong.com>
2016-08-18 14:47:09 +0800

    update wx config

--- application/library/LaneWeChat/config.php
+++ application/library/LaneWeChat/config.php
@@ -13,13 +13,13 @@ define('LANEWECHAT_VERSION_DATE', '2014-11-05');
 /*
  * 服务器配置，详情请参考@link http://mp.weixin.qq.com/wiki/index.php?title=接入指南
  */
-define("WECHAT_URL", 'http://wx.tongshijia.com/');
-define('WECHAT_TOKEN', 'ijSyHP3lNgPcDyfCL341');
-define('ENCODING_AES_KEY', "B6ZI9KpKzKor2k22DSohsA7e8T5wKq1nu6Ln4izBKLC");
+define("WECHAT_URL", 'http://www.tongshijia.com/weixin/msg_service');
+define('WECHAT_TOKEN', 'sUrjPDH8xus2d4JT');
+define('ENCODING_AES_KEY', "RlUScBFnD2oNaL9nbiAa0s648ghIy1OAVgD6bg4dZ7Q");
 
 /*
  * 开发者配置
  */
-define("WECHAT_APPID", 'wxddcc4ed5476aa61e');
-define("WECHAT_APPSECRET", '23e905ffe5443d35f652b8febd119ce0');
+define("WECHAT_APPID", 'wxca7838dcade4709c');
+define("WECHAT_APPSECRET", '79b787ec8f463eeb769540464c9277b2');
 //end weixin config
